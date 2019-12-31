25c9e358c44436557979cc4e0889c3aa67097903
Renan Bertoldo <renan.bertoldo@sofplan.com.br>
2019-12-31 11:25:17 -0300

    formulario com validações

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -8,17 +8,39 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  get f() {
+    return this.cadastro.controls;
+  }
 
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+  ngOnInit(): void {
+
+    this.cadastro = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]]
     });
 
+  }
+
+  salvar(): void {
+    this.cadastro.markAllAsTouched();
+    if (this.cadastro.invalid) {
+      return;
+    }
+
+    alert('SUCESSO!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void {
+    this.cadastro.reset();
   }
 
 }
