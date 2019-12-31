33272a746793fd5326a1687aa21b74171aa15c12
Renan Bertoldo <renan.bertoldo@sofplan.com.br>
2019-12-31 13:41:16 -0300

    serviço de validação

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -10,7 +11,8 @@ export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(public validacao: ValidarCamposService,
+              private fb: FormBuilder) { }
 
   get f() {
     return this.cadastro.controls;
---
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarCamposService {
+
+  constructor() { }
+
+  hasErrorValidar(control: AbstractControl, errorName: string): boolean {
+    if ((control.dirty || control.touched) && this.hasError(control, errorName)) {
+      return true;
+    }
+    return false;
+  }
+
+  hasError(control: AbstractControl, errorName: string): boolean {
+    return control.hasError(errorName);
+  }
+}
